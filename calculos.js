a5843c328f58578a261086bb2d1d37545d1c554e
Jacomo Araujogit config --global user.name Jacomo <jacomo68araujo@gmai.com>
2023-09-20 21:18:01 -0300

    closes#3 - Calculo funcionando

--- calculos.js
+++ calculos.js
@@ -1,39 +1,37 @@
-function obterTamBalao() {
-  balaoType = document.getElementsByName('balao')
-}
-
-
-
-function calculaCilindros (balaoType, unidadesBalao){
+function calculaCilindros (){
   event.preventDefault()
   let unidadesBalao = 0
   let balaoType = ""
   let nome = ""
-  let telefone = ""
-  let dataFesta = ""
   let cilindrosGH = 0
-  let precoCilindro = 10
+  let precoCilindro = 130
 
-  unidadesBalao = document.getElementById("unidadesBalao").value
-  balaoType = document.getElementById("balaoType").value
-  nome = document.getElementById("nome").value
-  telefone = document.getElementById("telefone").value
-  dataFesta = document.getElementById("dataFesta").value
+  unidadesBalao = document.getElementById("unidadesBalao").value;
+  nome = document.getElementById("nome").value;
+
+  const radioInputs = document.querySelectorAll('input[name="balao"]');
+  for (const radioInput of radioInputs) {
+  if (radioInput.checked) {
+    balaoType = radioInput.value;
+    break; // Sai do loop assim que encontrar o input selecionado.
+  }
+}
+
 
   if (balaoType == "balaoTam8"){
     cilindrosGH = unidadesBalao/100
   }
   if (balaoType == "balaoTam9"){
-    cilindrosGH == unidadesBalao/80
+    cilindrosGH = unidadesBalao/80
   }
   if (balaoType == "balaoTam10"){
     cilindrosGH = unidadesBalao/70
   }
 
-  if (checkbox.c) 
+ 
 
-  precoFinal = precoCilindro * cilindrosGH
+  const precoFinal = precoCilindro * cilindrosGH
 
-  document.getElementById("cilindrosGH").innerHTML = (`${nome}, Serão utilizados :${cilindrosGH.toFixed(2)}`)
-  document.getElementById("precoFinal").innerHTML = (`Valor gasto em Cilindros: ${precoFinal.toFixed(2)}`)
+  document.getElementById("cilindrosGH").innerHTML = (`${nome}, Serão utilizados: ${cilindrosGH.toFixed(2)} Cilindros`)
+  document.getElementById("precoFinal").innerHTML = (`Valor total em Cilindros: R$${precoFinal.toFixed(2)}`)
 }
